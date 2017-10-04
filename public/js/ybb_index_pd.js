bc4b43f6cffd2001dee53a3f282c9d4e88166253
yuanbingbing <1210054736@qq.com>
2017-10-04 22:31:56 +0800

    保存一下

--- public/js/ybb_index_pd.js
+++ public/js/ybb_index_pd.js
@@ -6,6 +6,39 @@ $(function(){
 		var index = $(this).parent().index();
 		$('.yingczang').removeClass('yingczang');
 		$('.zhuye').eq(index).addClass('yingczang');
-	})
+	});
 
+	//下拉框开始
+	$('.xiajiantou').click(function(){
+		$(this).children().children().toggleClass('icon-arrow-top');
+		$('.xialakuagn').toggleClass('kuangzi');
+	});
+	$('.xialakuagn ul li').click(function(){
+		var s = $(this).text();
+		$('.newimgs span').text(s);
+		$('.xialakuagn').toggleClass('kuangzi');
+	});
+	//点击出现遮罩层
+	$('.newxiagnce').click(function(){
+		var Wk = $(document).width();
+		var Hk = $(document).height();
+		//设置遮罩层的宽高
+		$('.zhezc').height(Hk);
+		$('.zhezc').width(Wk);
+		$('.zhezc').show();
+		var kk = $(window).width()/2;
+		var sk = $(window).height()/2;
+		var kuan = $('.clearInput').width()/2;
+		$('.clearInput').css({
+			left:kk-kuan,
+			top:sk
+		});
+		$('.zhezc').click(function(e){
+			e.stopPropagation();
+			$(this).hide();
+		});
+		$('.quxiaoxiao').click(function(){
+			$('.zhezc').hide();
+		})
+	})
 })
