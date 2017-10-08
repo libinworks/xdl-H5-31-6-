1267f6af8663f819ec69de14e584f390dceaf133
libinwords <1377715813@qq.com>
2017-10-08 17:13:19 +0800

    完成

--- controllers/index.js
+++ controllers/index.js
@@ -16,6 +16,30 @@ index.index = function(req, res) {
 	})
 	// res.render('index');
 }
+<<<<<<< HEAD
+=======
+
+index.help = function(req,res){
+	userModel.findOne({status : '1'},function(err,data){
+		if(!err && data){
+			// 分配数据
+			res.render('lb_help',{data:data,st:1});
+		}else{
+			res.render('lb_help',{st:0});
+		}
+	})
+}
+index.personalPage = function(req,res){
+	userModel.findOne({status : '1'},function(err,data){
+		if(!err && data){
+			// 分配数据
+			res.render('lb_self',{data:data,st:1});
+		}else{
+			res.render('lb_self',{st:0});
+		}
+	})
+}
+>>>>>>> 完成
 // 加载用户注册页面
 // index.reg = function(req, res) {
 	
@@ -60,6 +84,7 @@ index.checkUser = function(req,res){
 		}
 	})
 }
+<<<<<<< HEAD
 
 // 登录成功
 index.loginSuccess = function(req,res){
@@ -90,10 +115,45 @@ index.checkUserName = function(req,res){
 		}else{
 			// 该用户不存在 可用
 			res.send('ok');
+=======
+
+// 登录成功
+index.loginSuccess = function(req,res){
+	var uname = req.query.uname.trim();
+	userModel.findOne({uname : uname},function(err,data){
+		if(!err && data){
+			// 分配数据
+			res.render('header',{data:data});
+>>>>>>> 完成
 		}
 	})
 }
 
+<<<<<<< HEAD
+=======
+// 注册 验证用户名
+index.checkUserName = function(req,res){
+	var uname = req.query.uname.trim();
+	if(uname == ''){
+		// 发送响应
+		res.send('isNull')
+
+		// 终止程序
+		return;
+	}
+	// 将用户提交过来的数据，与数据库中现有的数据进行对比
+	userModel.findOne({uname:uname},function(err,data){
+		if(!err && data){
+			// 说明账户已经存在
+			res.send('used');
+		}else{
+			// 该用户不存在 可用
+			res.send('ok');
+		}
+	})
+}
+
+>>>>>>> 完成
 //处理用户注册的数据
 index.reg = function(req,res){
 
--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,18 @@ var express = require('express');
 var router = express.Router();
 
 // 加载控制器
+<<<<<<< HEAD
 var index = require('../controllers/index')
+=======
+var index = require('../controllers/index');
+>>>>>>> 完成
 
 /* GET home page. */
 router.get('/', index.index);
-
+//帮助
+router.get('/help', index.help);
+//个人主页
+router.get('/personalPage', index.personalPage);
 // 登录验证
 router.post('/checkUser', index.checkUser);
 
@@ -22,5 +29,23 @@ router.post('/reg',index.reg);
 // 退出登录
 router.get('/lognUp',index.lognUp);
 
+<<<<<<< HEAD
+// 登录验证
+router.post('/checkUser', index.checkUser);
+
+// 登录成功
+router.get('/loginSuccess',index.loginSuccess)
+
+// 注册 验证用户名
+router.get('/checkUserName',index.checkUserName);
+
+// 处理用户注册的数据
+router.post('/reg',index.reg);
+
+// 退出登录
+router.get('/lognUp',index.lognUp);
+
+=======
+>>>>>>> 完成
 
 module.exports = router;
